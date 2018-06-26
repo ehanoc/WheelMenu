5120210d93a238d67d925c37e598bf03b8140109
ehanoc <bmartins.6018@gmail.com>
2018-06-26 13:43:38 +0100

    credit to original author for circlelayout used

--- app/src/main/java/com/ehanoc/menuwheel/view/CircleLayout.kt
+++ app/src/main/java/com/ehanoc/menuwheel/view/CircleLayout.kt
@@ -13,6 +13,23 @@ import android.widget.LinearLayout
 import com.ehanoc.menuwheel.R
 import java.util.HashSet
 
+/*
+ * Copyright dmitry.zaicew@gmail.com Dmitry Zaitsev
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+// Changed the original class to fit the requirements and to Kotlin language
 class CircleLayout @SuppressLint("NewApi")
 constructor(context: Context, attrs: AttributeSet?) : ViewGroup(context, attrs) {
 
